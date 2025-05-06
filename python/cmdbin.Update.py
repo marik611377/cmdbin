524215c27e8ca4a5bdb8e455c4a9e01bd3fd1dfa
marik611377 <marik061103@gmail.com>
2025-05-06 22:13:12 +1000

    commit #3 v1.3

--- python/cmdbin.Update.py
+++ python/cmdbin.Update.py
@@ -1,8 +1,10 @@
-import os
-from os import getcwd
 import requests
+
+version = "1.3\n"
 
 url = "https://raw.githubusercontent.com/marik611377/cmdbin/refs/heads/main/version.cmdbin"
 r = requests.get(url)
 
-print("Got " + r.text)
+if version != r.text:
+    print("Your version of CMDbin is outdated. Please download the latest version.")
+    print("https://github.com/marik611377/cmdbin")
